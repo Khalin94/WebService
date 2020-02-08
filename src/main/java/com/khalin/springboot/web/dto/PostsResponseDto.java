7bb53d386c4a66face608fe7dbb927ed39828bba
khalin <mpzx1102@gmail.com>
2020-02-08 21:10:29 +0900

    add domain, service, controller, Test

---
+++ src/main/java/com/khalin/springboot/web/dto/PostsResponseDto.java
@@ -0,0 +1,19 @@
+package com.khalin.springboot.web.dto;
+
+import com.khalin.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity){
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content  = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+}
