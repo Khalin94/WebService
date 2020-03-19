ebe6b6c464d1967f6debfa869184e4b0eca722d8
khalin <mpzx1102@gmail.com>
2020-03-19 22:09:08 +0900

    add test separate JpaAuditing and more~

--- src/main/java/com/khalin/springboot/Application.java
+++ src/main/java/com/khalin/springboot/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
+//@EnableJpaAuditing  :: springBootApplication과 EnableJpaAuditing 분리
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- src/main/java/com/khalin/springboot/web/IndexController.java
+++ src/main/java/com/khalin/springboot/web/IndexController.java
@@ -1,5 +1,8 @@
 package com.khalin.springboot.web;
 
+import com.khalin.springboot.config.auth.LoginUser;
+import com.khalin.springboot.config.auth.dto.SessionUser;
+import com.khalin.springboot.domain.user.User;
 import com.khalin.springboot.service.PostsService;
 import com.khalin.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
 import javax.websocket.server.PathParam;
 
 @RequiredArgsConstructor
@@ -15,10 +19,16 @@ import javax.websocket.server.PathParam;
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts", postsService.findAllDesc());
+
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
+
         return "index";
     }
 
